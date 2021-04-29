9e7c5a050c0a9225cc2d4973e28eab3da5af7b37
mohamedmatrouh <mohamed.matrouh16@gmail.com>
2021-04-30 00:07:28 +0200

    deleted test repos

--- src/main/java/com/ensas/ebanking/repositories/PaymentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ensas.ebanking.repositories;
-
-import com.ensas.ebanking.entities.Payment;
-
-import javax.transaction.Transactional;
-
-@Transactional
-public interface PaymentRepository extends  TransactionBaseRepository<Payment>{
-}
